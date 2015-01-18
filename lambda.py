61b5db9fdc29c01633e5b48e3181b5c360746871
huyilong <hu,yilong@wustl.edu>
2015-01-18 10:45:29 -0600

    initial commit

---
+++ lambda.py
@@ -0,0 +1,9 @@
+pairs = [(1,"one"),(2,"two"),(3,"three"),(4,"four")]
+pairs.sort(key = lambda pair:pair[1])
+print pairs
+
+#using python in cmd
+#using ctr + d to quit
+
+#appledembp:desktop apple$ python lambda.py 
+#[(4, 'four'), (1, 'one'), (3, 'three'), (2, 'two')]
